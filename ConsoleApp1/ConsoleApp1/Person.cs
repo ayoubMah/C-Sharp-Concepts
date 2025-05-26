fb8d455524f2515b28b0735b0dba04d4d513cc1e
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-05-26 15:26:19 +0200

    play with cs

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+
+    public class Person
+    {
+        public string name;
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public void intreduce(string to)
+        {
+            Console.WriteLine("HI {0}, I am {1} ", to , name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ahmed");
+            //or var person = new Person("Ahmed");  
+            person.intreduce("ayoub");
+        }
+    }
+        
+}
